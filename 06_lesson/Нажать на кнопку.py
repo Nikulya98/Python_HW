d3e900c25eb0799da8b32e8902288f83f7057933
Nikulya98 <veron98.21@mail.ru>
2025-02-07 02:01:06 +0300

    My sixth commit

---
+++ 06_lesson/Нажать на кнопку.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+driver = webdriver.Chrome()
+driver.get("http://uitestingplayground.com/ajax")
+
+button = driver.find_element(
+    By.XPATH, "//button[contains(@class, 'btn-primary')]"
+)
+button.click()
+
+wait = WebDriverWait(driver, 10)
+message = wait.until(
+    EC.visibility_of_element_located((By.CSS_SELECTOR, ".bg-success"))
+)
+
+print(message.text)
+
+driver.quit()
