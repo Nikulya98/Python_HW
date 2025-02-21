3a35779b4af80bfdd84d670799408ad19b4d9232
Nikulya98 <veron98.21@mail.ru>
2025-02-21 23:02:05 +0300

    My eighth commit

--- 07_lesson/test_data_form/test_data_form.py
+++ 07_lesson/test_data_form/test_data_form.py
@@ -20,6 +20,10 @@ def test_fill_form(driver):
         "https://bonigarcia.dev/selenium-webdriver-java/data-types.html"
     )
 
+    WebDriverWait(driver, 15).until(
+        EC.presence_of_element_located((By.NAME, "first-name"))
+    )
+
     page.fill_form(
         first_name="Иван",
         last_name="Петров",
@@ -34,41 +38,37 @@ def test_fill_form(driver):
 
     page.submit_form()
 
-    WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.NAME, "zip-code"))
+    WebDriverWait(driver, 15).until(
+        EC.visibility_of_element_located((By.ID, "zip-code"))
     )
 
-    WebDriverWait(driver, 10).until(
-        EC.attribute_contains((By.NAME, "zip-code"), "class", "is-invalid")
+    assert "alert-danger" in page.get_element_attribute(
+        (By.ID, "zip-code"), "class"
     )
-
-    assert "is-invalid" in page.get_element_attribute(
-        page.ZIP_CODE, "class"
+    assert "alert-success" in page.get_element_attribute(
+        (By.ID, "first-name"), "class"
     )
-    assert "is-valid" in page.get_element_attribute(
-        page.FIRST_NAME, "class"
+    assert "alert-success" in page.get_element_attribute(
+        (By.ID, "last-name"), "class"
     )
-    assert "is-valid" in page.get_element_attribute(
-        page.LAST_NAME, "class"
+    assert "alert-success" in page.get_element_attribute(
+        (By.ID, "address"), "class"
     )
-    assert "is-valid" in page.get_element_attribute(
-        page.ADDRESS, "class"
+    assert "alert-success" in page.get_element_attribute(
+        (By.ID, "e-mail"), "class"
     )
-    assert "is-valid" in page.get_element_attribute(
-        page.EMAIL, "class"
+    assert "alert-success" in page.get_element_attribute(
+        (By.ID, "phone"), "class"
     )
-    assert "is-valid" in page.get_element_attribute(
-        page.PHONE, "class"
+    assert "alert-success" in page.get_element_attribute(
+        (By.ID, "city"), "class"
     )
-    assert "is-valid" in page.get_element_attribute(
-        page.CITY, "class"
+    assert "alert-success" in page.get_element_attribute(
+        (By.ID, "country"), "class"
     )
-    assert "is-valid" in page.get_element_attribute(
-        page.COUNTRY, "class"
+    assert "alert-success" in page.get_element_attribute(
+        (By.ID, "job-position"), "class"
     )
-    assert "is-valid" in page.get_element_attribute(
-        page.JOB_POSITION, "class"
-    )
-    assert "is-valid" in page.get_element_attribute(
-        page.COMPANY, "class"
+    assert "alert-success" in page.get_element_attribute(
+        (By.ID, "company"), "class"
     )
